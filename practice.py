6fc238edec5d0e8cd7cbaa02190534a89757a9c1
Bilal Ashraf <55201628+bilal255@users.noreply.github.com>
2019-10-16 21:34:31 +0500

    simple calculator.py

--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
+#This program is a basic arithmatic calculator
+#Defining caluculator function 
 def calculator (num1 , num2 , oper):
     if(oper == '+'):
         return num1+num2
@@ -10,9 +12,15 @@ def calculator (num1 , num2 , oper):
     else:
         return num1%num2
 
-a = 10
-b = 10
-op = '*'
+    
+#declaring two vars and one operator character variable and operator
+a = int(input("Enter 2st number"))
+b = int (input("Enter 2nd Number"))
+op = char(input("Enter the operator")
+
+#calling calculator function to calculatr the result of expression and printing result
 result = calculator(a ,b ,op)
+ 
+#Prinintg result
 print(result)
 
