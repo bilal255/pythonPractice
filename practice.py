1fbeec582d45f221119180b23d1a8a03bf07dbff
SHOAIB-RAFIQ <55207086+SHOAIB-RAFIQ@users.noreply.github.com>
2019-10-21 15:25:48 +0500

    Shoaib2

--- practice.py
+++ practice.py
@@ -11,7 +11,37 @@ def calculator (num1 , num2 , oper):
         return num1/num2
     else:
         return num1%num2
-
+#INCLUDE Irvine32.inc
+.data
+msg1 dword "you are eligible",0
+msg2 dword "you are not eligible",0
+b dword "bsc",0
+a dword 22
+age dword ?
+qual dword ?
+.code
+mov eax,0
+call readint
+mov age,eax
+call writestring
+mov edx,offset qual
+cmp age,a
+jmp m2
+cmp qual,b
+je m1
+m2:
+mov edx,offset b
+cmp qual,b
+jmp skip
+m1: 
+mov edx,offset msg1
+call writestring
+skip:
+mov edx,offset msg2
+call writestring
+exit 
+main endp
+end main
     
 #declaring two vars and one operator character variable and operator
 a = int(input("Enter 2st number"))
